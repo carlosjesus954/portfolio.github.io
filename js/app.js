5da01d677aafd03eeb0b9218dcee5738fc337b63
Carlos jesús Morales <95299663+carlosjesus954@users.noreply.github.com>
2022-10-27 12:52:38 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 // Variables
 const menu = document.querySelector('.nav__menu')
 const nav__ul = document.querySelector('.nav__ul')
+const submit = document.querySelector('.form__submit')
+const form = document.querySelector('.form')
+const button__enviame = document.querySelector('.enviame')
 // Eventos
 menu.addEventListener('click', mostrarMenu)
-
+form.addEventListener('submit', enviarFormulario)
 // Funciones
 function mostrarMenu(){
     console.log('hola');
     nav__ul.classList.toggle('activo')
+}
+
+function enviarFormulario (evento){
+    evento.preventDefault()
+    const formulario = new FormData(this)
+    console.log(formulario.get('nombre'));
+    button__enviame.setAttribute('href', `mailto:carlosmoralessoy@gmail.com?subject=Nombre : ${formulario.get('nombre')}, Email: ${formulario.get('email')}&body=${formulario.get('mensaje')}`)
+    button__enviame.click()
 }
