81a22737c51f7c58bd850707d94b4e4cadad9471
Carlos jesús Morales <95299663+carlosjesus954@users.noreply.github.com>
2022-10-27 15:00:25 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 // Variables
 const menu = document.querySelector('.nav__menu')
 const nav__ul = document.querySelector('.nav__ul')
-const submit = document.querySelector('.form__submit')
+const nav__close = document.querySelector('.nav__close')
+const nav__icons = document.querySelector('.nav__icons')
 const form = document.querySelector('.form')
 const button__enviame = document.querySelector('.enviame')
 // Eventos
 menu.addEventListener('click', mostrarMenu)
 form.addEventListener('submit', enviarFormulario)
+nav__close.addEventListener('click', cerrarMenu)
 // Funciones
+
 function mostrarMenu(){
     console.log('hola');
     nav__ul.classList.toggle('activo')
+    nav__icons.classList.toggle('disable')
+}
+
+function cerrarMenu(){
+
+    nav__ul.classList.toggle('activo')
+    nav__icons.classList.toggle('disable')
 }
 
 function enviarFormulario (evento){
