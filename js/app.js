9cce5341dc69819136e0fd060956197be6eea732
Carlos Jesús Morales <95299663+carlosjesus954@users.noreply.github.com>
2022-11-15 11:12:29 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,35 +1,33 @@
 // Variables
 const menu = document.querySelector('.nav__menu')
-const nav__ul = document.querySelector('.nav__ul')
-const nav__close = document.querySelector('.nav__close')
-const nav__icons = document.querySelector('.nav__icons')
+const navUl = document.querySelector('.nav__ul')
+const navClose = document.querySelector('.nav__close')
+const navIcons = document.querySelector('.nav__icons')
 const form = document.querySelector('.form')
-const button__enviame = document.querySelector('.enviame')
-const nav__a = document.querySelectorAll('.nav__a')
+const buttonEnviame = document.querySelector('.enviame')
+const navA = document.querySelectorAll('.nav__a')
 // Eventos
 menu.addEventListener('click', mostrarMenu)
 form.addEventListener('submit', enviarFormulario)
-nav__close.addEventListener('click', cerrarMenu)
-nav__a.forEach((e)=>{
+navClose.addEventListener('click', cerrarMenu)
+navA.forEach((e)=>{
     e.addEventListener('click', cerrarMenu)
 })
 // Funciones
 
 function mostrarMenu(){
-    console.log('hola');
-    nav__ul.classList.toggle('activo')
+    navUl.classList.toggle('activo')
 }
 
 function cerrarMenu(){
 
-    nav__ul.classList.toggle('activo')
+    navUl.classList.toggle('activo')
 
 }
 
 function enviarFormulario (evento){
     evento.preventDefault()
     const formulario = new FormData(this)
-    console.log(formulario.get('nombre'));
-    button__enviame.setAttribute('href', `mailto:carlosmoralessoy@gmail.com?subject=Nombre : ${formulario.get('nombre')}, Email: ${formulario.get('email')}&body=${formulario.get('mensaje')}`)
-    button__enviame.click()
+    buttonEnviame.setAttribute('href', `mailto:carlosmoralessoy@gmail.com?subject=Nombre : ${formulario.get('nombre')}, Email: ${formulario.get('email')}&body=${formulario.get('mensaje')}`)
+    buttonEnviame.click()
 }
